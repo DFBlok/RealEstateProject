1e084bd7464effdae7f1ee9fe5cf620254af2394
DFBlok <s219392862@mandela.ac.za>
2024-09-23 00:22:23 +0200

    home page image

---
+++ components/Home/Hero/Hero.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Hero = () => {
+  return (
+    <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative  bg-cover bg-center hero-section">
+      <h1>Hero </h1>
+    </div>
+  );
+};
+
+export default Hero;
