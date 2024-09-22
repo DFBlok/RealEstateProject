58a0dddff13eb58076077ef867347d74a2d097db
DFBlok <s219392862@mandela.ac.za>
2024-09-23 00:25:08 +0200

    black overlay added

--- components/Home/Hero/Hero.tsx
+++ components/Home/Hero/Hero.tsx
@@ -3,7 +3,8 @@ import React from "react";
 const Hero = () => {
   return (
     <div className="w-full pt-[4vh] md:pt-[12vh] h-screen bg-[#0f0715] overflow-hidden relative  bg-cover bg-center hero-section">
-      <h1>Hero </h1>
+      {/* Black overlay */}
+      <div className="absolute bg-black opacity-70 inset-0"></div>
     </div>
   );
 };
