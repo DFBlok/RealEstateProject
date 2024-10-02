254f23907cad0d07aa63eeaf0a8b2780e7a383ab
DFBlok <s219392862@mandela.ac.za>
2024-10-02 12:26:38 +0200

    added the multi carousel

--- components/Home/CityProperties/CityProperties.tsx
+++ components/Home/CityProperties/CityProperties.tsx
@@ -1,11 +1,15 @@
 import SectionHeading from "@/components/Helper/SectionHeading";
 import React from "react";
+import CitySlider from "./CitySlider";
 
 const CityProperties = () => {
   return (
     <div className="pt-16 pb-16">
       <div className="w-[80%] mx-auto">
         <SectionHeading heading="Explor City Properties" />
+        <div className="mt-10 md:mt16 ">
+          <CitySlider />
+        </div>
       </div>
     </div>
   );
