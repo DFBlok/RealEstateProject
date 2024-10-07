e17cacbbbbf47ec9d02be181dab2ab33f4ef5612
DFBlok <s219392862@mandela.ac.za>
2024-10-07 21:45:36 +0200

    review slider with card added

--- components/Home/Reviews/Reviews.tsx
+++ components/Home/Reviews/Reviews.tsx
@@ -1,11 +1,15 @@
 import SectionHeading from "@/components/Helper/SectionHeading";
 import React from "react";
+import ReviewSlider from "./ReviewSlider";
 
 const Reviews = () => {
   return (
     <div className="pt-16 pb-16 bg-[#fef7f6]">
       <div className="w-[80%] mx-auto">
         <SectionHeading heading="Our Client Reviews" />
+        <div>
+          <ReviewSlider />
+        </div>
       </div>
     </div>
   );
