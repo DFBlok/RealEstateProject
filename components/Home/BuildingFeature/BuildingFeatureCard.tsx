19a69ac21202dde1aa5b6fbce0611b7f772f4e25
DFBlok <s219392862@mandela.ac.za>
2024-10-07 17:18:31 +0200

    added the image inside the Tilt section

--- components/Home/BuildingFeature/BuildingFeatureCard.tsx
+++ components/Home/BuildingFeature/BuildingFeatureCard.tsx
@@ -1,5 +1,7 @@
+"use client";
 import React from "react";
-
+import Image from "next/image";
+import Tilt from "react-parallax-tilt";
 type Props = {
   building: {
     id: number;
@@ -10,7 +12,18 @@ type Props = {
 };
 
 const BuildingFeatureCard = ({ building }: Props) => {
-  return <div>BuildingFeatureCard</div>;
+  return (
+    <Tilt className="rounded-lg shadow-md p-4 bg-white">
+      <div className="w-16 h-16 ml-auto flex items-center justify-center flex-col bg-gray-100 rounded-full">
+        <Image
+          src={building.image}
+          alt={building.title}
+          width={40}
+          height={40}
+        />
+      </div>
+    </Tilt>
+  );
 };
 
 export default BuildingFeatureCard;
