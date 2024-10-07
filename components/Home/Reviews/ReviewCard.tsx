00986db3ef383c745cd90648a84db4df0233fe45
DFBlok <s219392862@mandela.ac.za>
2024-10-07 21:48:44 +0200

    added quote icon

--- components/Home/Reviews/ReviewCard.tsx
+++ components/Home/Reviews/ReviewCard.tsx
@@ -1,7 +1,12 @@
 import React from "react";
+import { FaQuoteRight } from "react-icons/fa";
 
 const ReviewCard = () => {
-  return <div>ReviewCard</div>;
+  return (
+    <div className="bg-white rounded-lg m-3 p-6 relative">
+      <FaQuoteRight className="w-8 h-8 absolute top-4 right-4 text-red-600 opacity-20" />
+    </div>
+  );
 };
 
 export default ReviewCard;
