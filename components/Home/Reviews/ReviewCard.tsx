ebba89896cc0cdc081fa0383690eb24ab927e48d
DFBlok <s219392862@mandela.ac.za>
2024-10-07 21:52:08 +0200

    added rating stars icons

--- components/Home/Reviews/ReviewCard.tsx
+++ components/Home/Reviews/ReviewCard.tsx
@@ -1,10 +1,17 @@
 import React from "react";
-import { FaQuoteRight } from "react-icons/fa";
+import { FaQuoteRight, FaStar } from "react-icons/fa";
 
 const ReviewCard = () => {
   return (
     <div className="bg-white rounded-lg m-3 p-6 relative">
       <FaQuoteRight className="w-8 h-8 absolute top-4 right-4 text-red-600 opacity-20" />
+      <div className="mt-6 items-center flex">
+        <FaStar className="w-4 h-4 text-yellow-600" />
+        <FaStar className="w-4 h-4 text-yellow-600" />
+        <FaStar className="w-4 h-4 text-yellow-600" />
+        <FaStar className="w-4 h-4 text-yellow-600" />
+        <FaStar className="w-4 h-4 text-yellow-600" />
+      </div>
     </div>
   );
 };
