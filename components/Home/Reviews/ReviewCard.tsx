055aa2696a091e0118e7f6bcbc9999c985d9a434
DFBlok <s219392862@mandela.ac.za>
2024-10-07 22:04:04 +0200

    added the line, client image, name and job

--- components/Home/Reviews/ReviewCard.tsx
+++ components/Home/Reviews/ReviewCard.tsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { FaQuoteRight, FaStar } from "react-icons/fa";
+import Image from "next/image";
 
 type Props = {
   review: {
@@ -23,6 +24,20 @@ const ReviewCard = ({ review }: Props) => {
         <FaStar className="w-4 h-4 text-yellow-600" />
       </div>
       <p className="mt-6 text-base">{review.review}</p>
+      <div className="w-full h-[1.2px] bg-gray-600 opacity-10 mt-6 mb-6"></div>
+      <div className="flex items-center space-x-4">
+        <Image
+          src={review.userImage}
+          alt={review.name}
+          height={50}
+          width={50}
+          className="rounded-full"
+        />
+        <div>
+          <h1 className="font-bold text-gray-800">{review.name}</h1>
+          <p className="text-sm text-gray-500">{review.profession}</p>
+        </div>
+      </div>
     </div>
   );
 };
